356d71d0dfc97bc2d9f4e92bbe9418c4fbbcca5e
mody258963 <122531523+mody258963@users.noreply.github.com>
2023-02-15 13:33:36 +0200

    fgh
    fg

--- lib/home page/inkwall.dart
+++ lib/home page/inkwall.dart
@@ -8,100 +8,16 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'movie.dart';
 
-/// A single movie row.
-class MovieItem extends StatelessWidget {
-  MovieItem(this.movie, {Key? key}) : super(key: key);
-
-  final Movie? movie;
-
-  /// Returns the movie poster.
-  Widget get poster {
-    return SizedBox(
-      width: 100,
-      child: Image.network(movie!.image),
-    );
-  }
-
-  /// Returns movie details.
-  Widget get details {
-    return Padding(
-      padding: const EdgeInsets.only(left: 8, right: 8),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          location,
-          expectedrent,
-          expectedprice,
-        ],
-      ),
-    );
-  }
-
-  /// Return the movie title.
-  Widget get location {
-    return Text(
-      '${movie!.location} (${movie!.expectedprice})',
-      style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-    );
-  }
-
-  /// Returns metadata about the movie.
-  Widget get expectedrent {
-    return Padding(
-      padding: const EdgeInsets.only(top: 8),
-      child: Row(
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(right: 8),
-            child: Text('Rated: ${movie!.expectedprice}'),
-          ),
-          Text('Runtime: ${movie!.expectedrent}'),
-        ],
-      ),
-    );
-  }
-
-  /// Returns a list of genre movie tags.
-  List<Widget> get genreItems {
-    return [
-      for (final location in movie!.detals ?? <String>[])
-        Padding(
-          padding: const EdgeInsets.only(right: 2),
-          child: Chip(
-            backgroundColor: Colors.lightBlue,
-            label: Text(
-              location,
-              style: const TextStyle(color: Colors.white),
-            ),
-          ),
-        )
-    ];
-  }
-
-  /// Returns all genres.
-  Widget get expectedprice {
-    return Padding(
-      padding: const EdgeInsets.only(top: 8),
-      child: Wrap(
-        children: genreItems,
-      ),
-    );
-  }
+class ThePoster extends StatefulWidget {
+  const ThePoster({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(bottom: 4, top: 4),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          poster,
-          Flexible(child: details),
-        ],
-      ),
-    );
-  }
+  State<ThePoster> createState() => _ThePosterState();
 }
 
-/// Displays and manages the movie 'like' count.
-
+class _ThePosterState extends State<ThePoster> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
--- lib/notifcatoin.dart
+++ lib/notifcatoin.dart
@@ -35,6 +35,41 @@ class notifcation extends StatelessWidget {
             ),
           ],
         ),
+        body: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 18, bottom: 10),
+                  child: Container(
+                    width: MediaQuery.of(context).size.width * 0.90,
+                    height: MediaQuery.of(context).size.height * 0.65,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Colors.black,
+                      boxShadow: [BoxShadow(color: Colors.white)],
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        Image.network(
+                          "https://www.shutterstock.com/shutterstock/photos/2070995981/display_1500/stock-vector-cyber-or-web-security-digital-data-protection-and-database-safety-bugs-correction-pentesting-and-2070995981.jpg",
+                          height: MediaQuery.of(context).size.height * 0.20,
+                          width: MediaQuery.of(context).size.width * 0.90,
+                          fit: BoxFit.cover,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
       debugShowCheckedModeBanner: false,
     );

1f28ed3b28aeac18ec53065bb459d5f2726df653
mody258963 <122531523+mody258963@users.noreply.github.com>
2023-02-18 19:15:05 +0200

    hh

--- lib/notifcatoin.dart
+++ lib/notifcatoin.dart
@@ -10,6 +10,7 @@ class notifcation extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Color.fromARGB(117, 53, 52, 52),
         appBar: AppBar(
           title: const Text(
             "Notifcation",
@@ -46,26 +47,69 @@ class notifcation extends StatelessWidget {
                     width: MediaQuery.of(context).size.width * 0.90,
                     height: MediaQuery.of(context).size.height * 0.65,
                     decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      color: Colors.black,
+                      borderRadius: BorderRadius.circular(15),
+                      color: Colors.white,
                       boxShadow: [BoxShadow(color: Colors.white)],
                     ),
                     child: Column(
                       mainAxisAlignment: MainAxisAlignment.start,
                       children: [
-                        ClipRRect(
-                          borderRadius: BorderRadius.circular(20),
+                        Row(
+                          mainAxisSize: MainAxisSize.max,
+                          children: [
+                            Expanded(
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.only(
+                                  bottomLeft: Radius.circular(0),
+                                  bottomRight: Radius.circular(0),
+                                  topLeft: Radius.circular(15),
+                                  topRight: Radius.circular(15),
+                                ),
+                                child: Image.network(
+                                  "https://www.shutterstock.com/shutterstock/photos/2070995981/display_1500/stock-vector-cyber-or-web-security-digital-data-protection-and-database-safety-bugs-correction-pentesting-and-2070995981.jpg",
+                                  height:
+                                      MediaQuery.of(context).size.height * 0.20,
+                                  width:
+                                      MediaQuery.of(context).size.width * 0.90,
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ),
+                          ],
                         ),
-                        Image.network(
-                          "https://www.shutterstock.com/shutterstock/photos/2070995981/display_1500/stock-vector-cyber-or-web-security-digital-data-protection-and-database-safety-bugs-correction-pentesting-and-2070995981.jpg",
-                          height: MediaQuery.of(context).size.height * 0.20,
-                          width: MediaQuery.of(context).size.width * 0.90,
-                          fit: BoxFit.cover,
+                        Padding(
+                          padding: const EdgeInsets.all(15),
+                          child: Row(
+                            children: [
+                              Container(
+                                height:
+                                    MediaQuery.of(context).size.height * 0.04,
+                                width: MediaQuery.of(context).size.width * 0.25,
+                                decoration: BoxDecoration(
+                                    color: Colors.white,
+                                    borderRadius: BorderRadius.circular(5),
+                                    border: Border.all(color: Colors.grey)),
+                              ),
+                              Padding(
+                                  padding: EdgeInsets.only(
+                                right: 5,
+                              )),
+                              Container(
+                                height:
+                                    MediaQuery.of(context).size.height * 0.04,
+                                width: MediaQuery.of(context).size.width * 0.25,
+                                decoration: BoxDecoration(
+                                    color: Colors.white,
+                                    borderRadius: BorderRadius.circular(5),
+                                    border: Border.all(color: Colors.grey)),
+                              ),
+                            ],
+                          ),
                         ),
                       ],
                     ),
                   ),
-                ),
+                )
               ],
             ),
           ),
